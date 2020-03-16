48ab62c26e770e3bc0b70c280f324fd6c1d9da6a
zhangyifan <zhang13612764562@163.com>
2020-03-16 16:53:33 +0800

    初始化项目提交到Git

---
+++ kefang-manage-dubbo-server/kefang-manage-dubbo-server-house-resources/kefang-manage-dubbo-server-house-resources-dubbo-service/src/main/java/cn/itkf/kefang/dubbo/server/pojo/BasePojo.java
@@ -0,0 +1,13 @@
+package cn.itkf.kefang.dubbo.server.pojo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+public class BasePojo implements Serializable {
+    private Date created;
+    private Date updated;
+
+}
